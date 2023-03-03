9d839faa9995c3989d5e30a9b54ad824a89ba762
Juan22345020 <126175834+Juan22345020@users.noreply.github.com>
2023-03-03 17:51:48 -0500

    Add files via upload

---
+++ Cual-de-los-tres-3-numeros-es-mayor.py
@@ -0,0 +1,31 @@
+# Cual numero es mayor
+
+print("------------------------------------------")
+print("---------Que numero es mayor--------------")
+print("------------------------------------------")
+
+
+# input
+
+n=int(input("Digite el valor 1 :"))
+m=int(input("Digite el valor 2 :"))
+x=int(input("Digite el valor 3 :"))
+
+if x==m==n :
+    print("los numeros son iguales" )
+
+
+
+
+
+
+
+
+if x>n :
+     print("el numero mayor es " + str(x)) 
+else:
+    print("el numero mayor es " + str(n))
+if x>m :
+        print("el numero mayor es " + str(x)) 
+else :
+    print("el numero mayor es " + str(m)) 
